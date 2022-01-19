91e82ca1badb8f2a9a82bb4436f406c8f6f79793
malavdin <malavdin05@gmail.com>
2022-01-19 21:08:05 +0300

    gghghg

---
+++ bin/malavdin111.dart
@@ -0,0 +1,70 @@
+void main(){
+
+}
+class Trian{
+  String punkt ='';
+  int vrem=0;
+  int nomer=0;
+}
+//   Character person= Character ('Jon' ,'man',17,76 );
+//   person.info();
+//
+//   Character masha= Character.masha(18, 55);
+//   masha.info();
+// }
+// class Character{
+//   String name = '' ;
+//   String gender = '';
+//   int age=0;
+//   int weight=0;
+//
+//   Character(String name1, String gender1,int age1,int weight1){
+//     name=name1;
+//     gender=gender1;
+//     age=age1;
+//     weight=weight1;
+//
+//
+//   }
+//   Character.masha(int age1,int weight1 ){
+//     name='Маша';
+//     gender='женский';
+//     age=age1;
+//     weight=weight1;
+//   }
+//   void info(){
+//     print('Это $name. Его пол $gender. Возраст $age. Вес $weight ');
+//   }
+// }
+//   WeatherDay pogoda= WeatherDay();
+//   pogoda.day = ' Пн , 17 января';
+//   pogoda.temp_d = 7;
+//   pogoda.temp_n= -3;
+//   pogoda.vlazh = 33.11;
+//   pogoda.print_info();
+//  }
+//   book book1= book();
+//   book1.name ='Война и мир';
+//   book1.avtor='Толстой';
+//   book1.date = 1863;
+//   book1.print_info();
+// }
+// class book{
+//   String name = '' ;
+//   String avtor = '';
+//   int date=0;
+//   void print_info(){
+//     print('Книга под названием $name , написанная автором $avtor изданная в $date году');
+//   }
+// }
+// class WeatherDay{
+//   String day='';
+//   int temp_d=0;
+//   int temp_n=0;
+//   double vlazh= 0;
+//   void print_info(){
+//     print(' Сегодня $day. температура днем состовляет :$temp_d а ночью : ,$temp_n. Влажность воздуха была равна : $vlazh');
+//   }
+
+
+// }
